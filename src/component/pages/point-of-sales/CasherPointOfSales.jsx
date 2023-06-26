17d27a4d78ff4b6856e1af073ca20e43d0ed4ddb
Cyrhina <cyrenemlumabas@gmail.com>
2023-06-26 14:25:16 +0800

    update ni cy

--- src/component/pages/point-of-sales/CasherPointOfSales.jsx
+++ src/component/pages/point-of-sales/CasherPointOfSales.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { FaPlusCircle } from "react-icons/fa";
-import { setIsAdd } from "../../../store/StoreAction.jsx";
 import { StoreContext } from "../../../store/StoreContext.jsx";
 import Footer from "../../partials/Footer.jsx";
 import Header from "../../partials/Header.jsx";
+import PageNotFound from "../../partials/PageNotFound.jsx";
 import ModalError from "../../partials/modals/ModalError.jsx";
 import ModalSuccess from "../../partials/modals/ModalSuccess.jsx";
-import ModalAddCasherPointOfSales from "./ModalAddCasherPointOfSales.jsx";
 import CasherPointOfSalesList from "./CasherPointOfSalesList.jsx";
 
 const CasherPointOfSales = () => {
   const { store, dispatch } = React.useContext(StoreContext);
+  const sccToken = JSON.parse(localStorage.getItem("sccToken"));
+
+  if (sccToken.isDev === true) {
+    return <PageNotFound />;
+  }
+  if (store.credentials.data.role_is_cashier !== 1) {
+    return <PageNotFound />;
+  }
   return (
     <>
       <Header />
