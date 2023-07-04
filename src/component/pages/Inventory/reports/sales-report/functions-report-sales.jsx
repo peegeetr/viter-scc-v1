66e1ae4aae5fd13d83153063523bc29c3470f6a7
Cyrhina <cyrenemlumabas@gmail.com>
2023-07-04 14:47:36 +0800

    update ni cy

--- src/component/pages/Inventory/reports/sales-report/functions-report-sales.jsx
+++ src/component/pages/Inventory/reports/sales-report/functions-report-sales.jsx
@@ -3,6 +3,7 @@ export const computeSccSales = (result) => {
   let totalOty = 0;
   let supplierPrice = 0;
   let sccPriceAmount = 0;
+  let sccPricePendingAmount = 0;
   let totalSupplierAmount = 0;
   let finalAmount = 0;
   let pendingAmount = 0;
@@ -14,14 +15,19 @@ export const computeSccSales = (result) => {
         totalSupplierAmount = totalOty * supplierPrice;
         sccPriceAmount =
           Number(item.orders_product_amount) - Number(item.sales_discount);
-        finalAmount += sccPriceAmount - totalSupplierAmount;
       }
+
       if (item.sales_is_paid === 0) {
         totalOty = Number(item.orders_product_quantity);
         supplierPrice = Number(item.orders_suplier_price);
         totalSupplierAmount = totalOty * supplierPrice;
-        sccPriceAmount =
+        sccPricePendingAmount =
           Number(item.orders_product_amount) - Number(item.sales_discount);
+      }
+      if (sccPriceAmount !== 0) {
+        finalAmount += sccPriceAmount - totalSupplierAmount;
+      }
+      if (sccPricePendingAmount !== 0) {
         pendingAmount += sccPriceAmount - totalSupplierAmount;
       }
     })
