9770a27e79ddb9360b14c139b8f94ce9f2349b95
Cyrhina <cyrenemlumabas@gmail.com>
2023-08-02 07:07:09 +0800

    update ni cy

--- src/component/pages/dashboard/cards/TopSellerCard.jsx
+++ src/component/pages/dashboard/cards/TopSellerCard.jsx
@@ -3,10 +3,8 @@ import TopSellerList from "../../Inventory/reports/top-seller/TopSellerList";
 const TopSellerCard = () => {
   return (
     <>
-      <div className="">
-        <div className="border mb-4 !pt-2 p-4 rounded-md">
-          <TopSellerList menu="overview" />
-        </div>
+      <div className="border mb-4 !pt-2 p-4 rounded-md">
+        <TopSellerList menu="overview" />
       </div>
     </>
   );
