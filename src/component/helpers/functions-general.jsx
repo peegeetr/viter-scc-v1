18558b8cf9858d46d7150527a97990519d83f7bb
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-08-25 10:28:15 +0800

    updating version

--- src/component/helpers/functions-general.jsx
+++ src/component/helpers/functions-general.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { setIsAdd } from "../../store/StoreAction.jsx";
 
 // Online URL Prod
-export const urlScc = "https://app.sambahayancoop.com";
-export const urlSccImg = "https://app.sambahayancoop.com/img";
+// export const urlScc = "https://app.sambahayancoop.com";
+// export const urlSccImg = "https://app.sambahayancoop.com/img";
 
 // // Online URL dev
-// export const urlScc = "https://dev.sambahayancoop.com";
-// export const urlSccImg = "https://dev.sambahayancoop.com/img";
+export const urlScc = "https://dev.sambahayancoop.com";
+export const urlSccImg = "https://dev.sambahayancoop.com/img";
 
 // // cy url
 // export const urlSccImg = "http://localhost/projects/viter-scc-v1/public/img";
