27a84f0b1171036dcfd9932177159d81f92a3341
Cyrhina <cyrenemlumabas@gmail.com>
2023-10-05 13:45:53 +0800

    update ni cy

--- src/component/helpers/functions-general.jsx
+++ src/component/helpers/functions-general.jsx
@@ -5,13 +5,13 @@ import { setIsAdd } from "../../store/StoreAction.jsx";
 // export const urlScc = "https://app.sambahayancoop.com";
 // export const urlSccImg = "https://app.sambahayancoop.com/img";
 
-// // // Online URL dev
-// export const urlScc = "https://dev.sambahayancoop.com";
-// export const urlSccImg = "https://dev.sambahayancoop.com/img";
+// // Online URL dev
+export const urlScc = "https://dev.sambahayancoop.com";
+export const urlSccImg = "https://dev.sambahayancoop.com/img";
 
-// // cy url
-export const urlSccImg = "http://localhost/projects/viter-scc-v1/public/img";
-export const urlScc = "http://localhost/projects/viter-scc-v1";
+// // // cy url
+// export const urlSccImg = "http://localhost/projects/viter-scc-v1/public/img";
+// export const urlScc = "http://localhost/projects/viter-scc-v1";
 
 // ONLINE DEV and LOCAL hris
 export const devBaseImgUrl = `${urlSccImg}`;
